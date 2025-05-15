d528b4d361c8f330f4fbaa071c1d5742b398ce41
lzt <490845291@qq.com>
2025-05-15 19:58:52 +0800

    1

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-import { createRequire } from 'module';   
+import { createRequire } from 'module';
 // 从node.js 14版及以上版本中，require作为COMMONJS的一个命令已不再直接支持使用，所以我们需要导入createRequire命令才可以
 const require = createRequire(import.meta.url);
 
@@ -11,10 +11,24 @@ const createWindow = () => {
         height: 700,
     })
 
-    mainWindow.loadURL("http://localhost:7500/");
+    // 开发环境使用本地服务器，生产环境使用打包后的文件
+    if (isDev) {
+        mainWindow.loadURL("http://localhost:7500/")
+        // 打开开发工具
+        mainWindow.webContents.openDevTools()
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+    }
 }
 
 // 在应用准备就绪时调用函数
 app.whenReady().then(() => {
     createWindow()
+})
+
+// 关闭所有窗口时退出应用
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
