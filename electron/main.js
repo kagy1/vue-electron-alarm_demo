891a2088cc33eeeb610715629c4338bd8d0cf088
lzt <490845291@qq.com>
2025-05-15 20:20:06 +0800

    1

--- electron/main.js
+++ electron/main.js
@@ -1,9 +1,18 @@
 import { createRequire } from 'module';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 // 从node.js 14版及以上版本中，require作为COMMONJS的一个命令已不再直接支持使用，所以我们需要导入createRequire命令才可以
 const require = createRequire(import.meta.url);
 
 const { app, BrowserWindow } = require('electron')
 const path = require("path")
+const isDev = process.env.NODE_ENV === 'development';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+console.log("__dirname", __dirname);
+
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
@@ -11,24 +20,14 @@ const createWindow = () => {
         height: 700,
     })
 
-    // 开发环境使用本地服务器，生产环境使用打包后的文件
     if (isDev) {
-        mainWindow.loadURL("http://localhost:7500/")
-        // 打开开发工具
-        mainWindow.webContents.openDevTools()
+        mainWindow.loadURL("http://localhost:7500/");
     } else {
-        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'))
+        mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
     }
 }
 
 // 在应用准备就绪时调用函数
 app.whenReady().then(() => {
     createWindow()
-})
-
-// 关闭所有窗口时退出应用
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
 })
