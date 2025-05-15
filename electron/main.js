2d8423d29f86ebf21c6e17a1dc17385871662704
lzt <490845291@qq.com>
2025-05-15 20:54:36 +0800

    1

--- electron/main.js
+++ electron/main.js
@@ -31,3 +31,10 @@ const createWindow = () => {
 app.whenReady().then(() => {
     createWindow()
 })
+
+// 关闭所有窗口时退出应用
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
